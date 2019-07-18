6d733bab8571247fa2afdf095a37159058edf275
Maximilian Ludvigsson <max@textual.se>
2019-07-18 19:43:00 +0200

    Bulk insert measurements

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from typing import List
+from datetime import datetime, timedelta
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Schema
 from databases import Database
+from asyncpg.exceptions import ForeignKeyViolationError
 
-from models import devices, measurements
+import models
 from settings import DATABASES
 
 
@@ -31,18 +33,31 @@ async def shutdown():
 # SCHEMAS #
 ###########
 
-
-class DeviceCreate(BaseModel):
+class DeviceBase(BaseModel):
     name: str
 
 
-class Device(DeviceCreate):
+class DeviceCreate(DeviceBase):
+    pass
+
+
+class Device(DeviceBase):
     id: int
 
 
-class MeasurementCreate(BaseModel):
-    device_id: int
+class Reading(BaseModel):
     reading: float
+    timedelta: int = Schema(..., description=(
+        'Milliseconds from reading the value to sending the request'
+    ))
+
+
+class MeasurementCreate(BaseModel):
+    readings: List[Reading]
+
+
+class Message(BaseModel):
+    message: str
 
 
 ##########
@@ -52,16 +67,29 @@ class MeasurementCreate(BaseModel):
 
 @app.get('/devices', response_model=List[Device])
 async def read_devices():
-    return await db.fetch_all(devices.select())
+    return await db.fetch_all(models.devices.select())
 
 
 @app.post('/devices', response_model=Device)
-async def create_device(device: DeviceCreate) -> Device:
-    id = await db.execute(devices.insert(device.dict()))
+async def create_device(device: DeviceCreate):
+    id = await db.execute(models.devices.insert(device.dict()))
     return {**device.dict(), 'id': id}
 
 
-@app.post('/measurements', response_model={})
-async def create_measurement(measurement: MeasurementCreate):
-    id = await db.execute(measurements.insert(measurement.dict()))
-    return {}
+@app.post('/device/{device_id}', response_model=Message)
+async def create_measurements(device_id: int, measurements: MeasurementCreate):
+    current_time = datetime.now()
+    to_insert = [{
+        'reading': measurement.reading,
+        'device_id': device_id,
+        'created_at': (
+            current_time + timedelta(milliseconds=measurement.timedelta)
+        ),
+    } for measurement in measurements.readings]
+
+    try:
+        await db.execute(models.measurements.insert(to_insert))
+        return {'message': 'Measurements successfully inserted'}
+    except ForeignKeyViolationError:
+        msg = 'No device with this id'
+        raise HTTPException(status_code=404, detail=msg)
