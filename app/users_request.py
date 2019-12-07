76fce9f440b36b5ec540b22da8f2d03e191cf8e3
Ludvigsson <adrian.ludvigsson@gmail.com>
2019-12-07 16:28:04 +0100

    pushed to production

--- app/users_request.py
+++ app/users_request.py
@@ -1,1 +1,14 @@
-def 
+from main import oauth2_scheme
+
+
+
+def fake_decode_token(token):
+    return User(
+        username=token + "fakedecoded", email="john@example.com", full_name="John Doe"
+    )
+
+
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    return user
