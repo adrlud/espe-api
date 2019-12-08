e97f0c6a2beb9264fb313f574ab1638026b7df9b
Ludvigsson <adrian.ludvigsson@gmail.com>
2019-12-08 22:48:12 +0100

    pushed to production

--- app/test.py
+++ app/test.py
@@ -28,7 +28,7 @@ class TestUser(unittest.TestCase):
         self.assertEqual(result2, data["alice"])
     
     def test_get_current_user():
-       print(users_request.get_current_user("hej"))
+       print(users_request.get_current_user())
 
 
 
