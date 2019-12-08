27030241adc04edf82489db068c1c7aa122acc96
Ludvigsson <adrian.ludvigsson@gmail.com>
2019-12-08 20:15:49 +0100

    pushed to production

--- logs.sh
+++ logs.sh
@@ -1,5 +1,6 @@
 ssh -T espe@144.202.14.56 << EOSSH
 cd espe-api/
-docker-compose logs
+docker-compose logs | grep "WARNING"
+docker-compose logs | grep "ERROR"
 logout
 
---
+++ test.py
@@ -0,0 +1,29 @@
+import unittest
+#target = __import__("app/users_request.py")
+#users_request = target.users_request
+from app import users_request 
+class TestUser(unittest.TestCase):
+    def test_get_user(self):
+        data =  {
+            "johndoe": {
+                "username": "johndoe",
+                "full_name": "John Doe",
+                "email": "johndoe@example.com",
+                "hashed_password": "fakehashedsecret",
+                "disabled": False,
+            },
+            "alice": {
+                "username": "alice",
+                "full_name": "Alice Wonderson",
+                "email": "alice@example.com",
+                "hashed_password": "fakehashedsecret2",
+                "disabled": True,
+                },
+            } 
+        result = users_request.get_user(data, "johndoe")
+        self.assertDictEqual(re, data["johndoe"])
+
+
+
+if __name__ == '__main__':
+    unittest.main()
