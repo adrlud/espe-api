6fad286925fcdcb5736c27f6c107a0bc4b20b7bf
Ludvigsson <adrian.ludvigsson@gmail.com>
2019-12-08 23:26:31 +0100

    pushed to production

--- app/main.py
+++ app/main.py
@@ -83,12 +83,13 @@ class Event(BaseModel):
 async def login(form_data: OAuth2PasswordRequestForm = Depends()):
     user_dict = fake_users_db.get(form_data.username)
     if not user_dict:
-        raise HTTPException(status_code =400, detail = "Incorrect credentials")
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
     user = UserInDB(**user_dict)
     hashed_password = fake_hash_password(form_data.password)
     if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect password")
-    return { "access_token": user.username, "token_type": "bearer"}
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
 
 @app.get("/users/me")
 async def read_users_me(current_user: User = Depends(get_current_active_user)):
